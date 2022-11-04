691cf5629a0db09da88d6e99997023adcbc34943
Mobile <agustin@getcharly.com>
2022-11-04 00:50:58 -0300

    enabled cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connect = require("./src/config/connect");
 const routes = require("./src/routes");
@@ -6,6 +7,7 @@ const routes = require("./src/routes");
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
